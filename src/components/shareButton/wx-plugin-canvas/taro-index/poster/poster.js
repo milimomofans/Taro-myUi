64570172e7812b17cf5d0c0c3b0272b545e37fe8
肖健鑫 <815310779@qq.com>
2020-06-13 16:12:26 +0800

    更改文件

---
+++ src/components/shareButton/wx-plugin-canvas/taro-index/poster/poster.js
@@ -0,0 +1,33 @@
+const defaultOptions = {
+  selector: '#poster'
+}
+
+function Poster(options = {}) {
+  options = {
+    ...defaultOptions,
+    ...options
+  }
+
+  const pages = getCurrentPages()
+  const ctx = pages[pages.length - 1]
+
+  let poster = options.poster ?
+    options.poster :
+    ctx.selectComponent(options.selector)
+  delete options.selector
+
+  return poster
+}
+
+Poster.create = (reset = false, posterComp) => {
+  const poster = Poster({
+    poster: posterComp
+  })
+  if (!poster) {
+    console.error('请设置组件的id="poster"!!!')
+  } else {
+    return poster._onCreate(reset)
+  }
+}
+
+export default Poster
