6429d97e904bb76d18838728fd4d3b268d78f283
肖健鑫 <815310779@qq.com>
2020-08-06 16:51:49 +0800

    更新无线滚动

--- dist/h5/components/infinite-scroll/index.js
+++ dist/h5/components/infinite-scroll/index.js
@@ -1,10 +1,11 @@
 import Nerv from "nervjs";
 import { Component } from "@tarojs/taro-h5";
-import { ScrollView, View } from "@tarojs/components";
+import { ScrollView, View, Image } from "@tarojs/components";
 // import { AtLoadMore } from "taro-ui"; // 之后看是否抄到water-ui里面
 import classNames from "classnames";
 import WtLoadMore from "../load-more/load-more";
 import '../../style/components/inifinite-scroll.scss';
+import { noDataIcon } from './imgs';
 class InfiniteScroll extends Component {
   constructor(props) {
     super(props);
@@ -19,8 +20,10 @@ class InfiniteScroll extends Component {
     return <ScrollView className={coverClass} scrollY scrollWithAnimation style={"height: 100vh;" + customStyle} scrollTop={0} lowerThreshold={lowerThreshold ? lowerThreshold : 80} onScrollToLower={this.onScrollToLower}>
         {showFiller && <View className="infinite-scroll-filler" />}
         {this.props.children}
-        <WtLoadMore status={loading ? "loading" : "noLoading"} />
-        {showBtmFiller && !hasMore && !isEmpty && <View className="no-more" />}
+        {isEmpty && hasMore ? <View className="noData">
+              <Image src={noDataIcon} mode="widthFix" />
+              <View>暂无数据</View> 
+            </View> : <WtLoadMore status={loading ? "loading" : "noLoading"} />}
       </ScrollView>;
   }
 }
--- src/components/infinite-scroll/index.tsx
+++ src/components/infinite-scroll/index.tsx
@@ -1,9 +1,10 @@
 import Taro, { Component } from "@tarojs/taro";
-import { ScrollView, View } from "@tarojs/components";
+import { ScrollView, View, Image } from "@tarojs/components";
 // import { AtLoadMore } from "taro-ui"; // 之后看是否抄到water-ui里面
 import classNames from "classnames";
 import WtLoadMore from "../load-more/load-more";
 import '../../style/components/inifinite-scroll.scss'
+import {noDataIcon} from './imgs'
 
 interface ComponentProps {
   isEmpty: boolean;
@@ -59,8 +60,16 @@ class InfiniteScroll extends Component<ComponentProps> {
       >
         {showFiller && <View className="infinite-scroll-filler" />}
         {this.props.children}
-        <WtLoadMore status={loading ? "loading" : "noLoading"} />
-        {showBtmFiller && !hasMore && !isEmpty && <View className="no-more" />}
+        {
+          isEmpty && hasMore ? (
+            <View className="noData">
+              <Image src={noDataIcon} mode='widthFix'/>
+              <View>暂无数据</View> 
+            </View>
+          ) : (
+            <WtLoadMore status={loading ? "loading" : "noLoading"} />
+          )
+        }
       </ScrollView>
     );
   }
